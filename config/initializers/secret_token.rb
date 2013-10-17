db0645547031a6d333a6aeebc34441b88887900f
EC2 Default User <ec2-user@ip-172-31-15-210.us-west-2.compute.internal>
2013-10-17 19:00:55 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlazorApp::Application.config.secret_key_base = '5d4da290bb227d65a0af6efc1266ae6dec07a9d24de6b38f1ccce255ced303cef99c0cdffe98771aa066e9d9674099a8e93d366eaae3978829fff4a184d5a4e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlazorApp::Application.config.session_store :cookie_store, key: '_flazorApp_session'
